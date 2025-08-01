9fb64f9dee3dc670d221856f030206c48814c37f
sena <gulozseven@gmail.com>
2025-08-01 22:45:00 +0300

    Update AnimalController.java

--- src/main/java/com/workintech/fswebs17d1/controller/AnimalController.java
+++ src/main/java/com/workintech/fswebs17d1/controller/AnimalController.java
@@ -4,11 +4,23 @@ package com.workintech.fswebs17d1.controller;
 
 //AnimalResource, AnimalRestController olarak da adlandırılabilir.
 
+import com.workintech.fswebs17d1.entity.Animal;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 //pom.xml'de dependecy olan spring-boot-starter-web paketinden geliyor.
 
-//default host name: 8585  --> application.properties kısmında öyle ayarlandığım için.
+//default host name: http://localhost:8585  --> application.properties kısmında öyle ayarlandığım için.
 
 @RestController
+@RequestMapping(path = "/workintech/animal ")
+//http://localhost:8585/workintech/animal --> AnimalController 
 public class AnimalController {
+    private Map<Integer, Animal> animalMap;
+
+
+    @PostConstruct
+
 }
