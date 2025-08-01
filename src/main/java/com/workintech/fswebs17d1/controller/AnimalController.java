68d5b700c6bc6d0c53a8b86508a5454a83a2d9f4
sena <gulozseven@gmail.com>
2025-08-01 22:40:39 +0300

    Controller and Entity Package

---
+++ src/main/java/com/workintech/fswebs17d1/controller/AnimalController.java
@@ -0,0 +1,14 @@
+package com.workintech.fswebs17d1.controller;
+//CONTROLLER PACKAGE2I DIŞ DÜNYAYA AÇTIĞIMIZ KISIMDIR.
+
+
+//AnimalResource, AnimalRestController olarak da adlandırılabilir.
+
+import org.springframework.web.bind.annotation.RestController;
+//pom.xml'de dependecy olan spring-boot-starter-web paketinden geliyor.
+
+//default host name: 8585  --> application.properties kısmında öyle ayarlandığım için.
+
+@RestController
+public class AnimalController {
+}
